4ab7f4c4313ad4916cfd71314d68f9379b7b5048
Scott Neibarger <scott.neibarger@gmail.com>
2025-06-17 17:27:22 -0400

    Re-adding noun_intersection_ranker.py.

---
+++ NounIntersectionRanker.py
@@ -0,0 +1,42 @@
+import logging
+from typing import List, Tuple, Set, Dict, Optional
+
+
+class NounIntersectionRanker:
+    def __init__(self, key_terms: Set[str], ontology: Optional[Set[str]]):
+        self.logger = logging.getLogger('OWL-to-KM.NounIntersectionRanker')
+        self.key_terms = key_terms
+        self.cyc_concepts = ontology
+        self.logger.info("Initialized NounIntersectionRanker with %d key terms", len(key_terms))
+
+    def extract_nouns(self, parse_tree: Dict) -> Set[str]:
+        nouns = set()
+        if "sentences" in parse_tree:
+            for sentence in parse_tree.get("sentences", []):
+                for token in sentence.get("tokens", []):
+                    word = token.get("word")
+                    pos = token.get("pos", "")
+                    if pos.startswith("N") and word is not None:
+                        nouns.add(word.lower())
+            self.logger.debug("Extracted %d nouns from parse tree", len(nouns))
+        else:
+            self.logger.warning("No 'sentences' key in parse tree")
+        return nouns
+
+    def rank_items(self, items: List[Tuple[str, Dict]]) -> List[Tuple[str, int]]:
+        self.logger.info("Ranking %d items", len(items))
+        ranked_items = []
+
+        for item_text, parse_tree in items:
+            if parse_tree and "parseTree" in parse_tree:
+                nouns = self.extract_nouns(parse_tree["parseTree"])
+                score = len(nouns.intersection(self.key_terms))
+                ranked_items.append((item_text, score))
+                self.logger.debug("Score for item '%s...': %d", item_text[:30], score)
+            else:
+                self.logger.warning("No parseTree found for item: %s...", item_text[:30])
+                ranked_items.append((item_text, 0))
+
+        ranked_items.sort(key=lambda x: x[1], reverse=True)
+        self.logger.info("Ranking complete; top score: %d", ranked_items[0][1] if ranked_items else 0)
+        return ranked_items
