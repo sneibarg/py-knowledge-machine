9d947d47da16f7277eed0b29af2b54c19053beae
Scott Neibarger <scott.neibarger@gmail.com>
2025-08-18 14:40:00 -0400

    Adding synset stuff.

--- processor/nlp/CycSynset.py
+++ processor/nlp/CycSynset.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from processor.nlp.Synset import Synset
 from service.CycLService import CycLService
 from service.NlpService import NlpService
@@ -7,14 +9,28 @@ class CycSynset(Synset):
     def __init__(self, term: str, pos: str, nlp_service: NlpService, cycl_service: CycLService):
         super().__init__(term, pos, nlp_service)
         self.cycl_service = cycl_service
-        self.predicates = None
-        self.functions = None
+        self.predicates = self.__init_predicates()
+        self.functions = self.__init_functions()
 
-    def __init_predicates(self):
-        query = f"(#$isa {self.term.capitalize()} #$Predicate)"
-        self.functions = self.cycl_service.query_sentence(query, mt_monad="BaseKB")
+    def __init_predicates(self) -> List:
+        query = f"(#$isa {self.term.capitalize()} #$Predicate)"  # returns Query is not proven when unlinked
+        answers = self.cycl_service.query_sentence(query, mt_monad="BaseKB")['answers']
+        if answers.keys() < 1:
+            return self.__link_predicates()
+        else:
+            return answers
 
-    def __init_functions(self):
+    def __init_functions(self) -> List:
         query = f"(#$isa {self.term.capitalize()} #$CollectionDenotingFunction)"
-        self.functions = self.cycl_service.query_sentence(query, mt_monad="BaseKB")
+        answers = self.cycl_service.query_sentence(query, mt_monad="BaseKB")['answers']
+        if answers.keys() < 1:
+            return self.__link_functions()
+        else:
+            return answers
+
+    def __link_predicates(self) -> List:
+        pass
+
+    def __link_functions(self) -> List:
+        pass
 
