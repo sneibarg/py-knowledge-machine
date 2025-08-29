86897af16c78e1f425bdf45b0c7738a579a32079
Scott Neibarger <scott.neibarger@gmail.com>
2025-08-28 20:01:05 -0400

    buh fixes

--- processor/nlp/CycSynset.py
+++ processor/nlp/CycSynset.py
@@ -4,6 +4,7 @@ from service.CycLService import CycLService
 from service.NlpService import NlpService
 from service.OllamaService import OllamaService
 
+model_name = "gpt-oss:20b"
 base_prompt = ("I am your automated ontology editor, and I am reviewing the results of a CycL query."
                "I will be given a list of OpenCyc instances. "
                "I understand that ?INSTANCE instances are a feature of the OpenCyc platform."
@@ -20,6 +21,7 @@ class CycSynset(Synset):
         self.term_comment = self.__init_term_comment()
         self.predicates = self.__init_predicates()
         self.functions = self.__init_functions()
+        self.model_name = "gpt-oss:20b"
 
     def __relevant(self, answers, instance_type) -> List:
         prompt = base_prompt.replace("?INSTANCE", instance_type)
@@ -27,30 +29,32 @@ class CycSynset(Synset):
             prompt = prompt.replace("?COMMENT", self.predicate_comment)
         else:
             prompt = prompt.replace("?COMMENT", self.function_comment)
-
-        text = f"The OpenCyc result set for {self.term} is listed as follows: {' '.join(answers)}.\n"
-        text = text + "I will reason through this list and return a much smaller list to assert to a different Microtheory.\n"
-        text = text + "As part of my reasoning process, I will determine whether the instance is a member of the upper, middle, or lower ontology."
-        text = text + "The selected instances are: "
-        return self.ollama_service.one_shot(text, prompt).replace("The selected instances are: ", "").split(" ")
+        text = f"The OpenCyc result set for {self.term} is listed as follows: {' '.join(answers.values())}.\n"
+        text = text + "I will reason through this list.\n"
+        text = text + "As part of my reasoning process, I will determine whether the instance is a member of the upper, middle, or lower ontology.\n"
+        text = text + "I will output the OpenCyc result set as three lines each of comma-separated (with a space after the comma) names without introducing any other text.\n"
+        print(f"Submitting Ollama request with the following prompt: \n{prompt}\n\nPrompt text is {text}")
+        # response = self.ollama_service.one_shot(model_name, text, prompt)
+        # print(f"The response was: {str(response)}")
+        return self.ollama_service.one_shot(model_name, text, prompt).split('\n')
 
     def __init_term_comment(self) -> str:
         query = f"(comment {self.term.capitalize()} ?TEXT)"
-        term_comment = self.cycl_service.query_sentence(query, mt_monad="EnglishMt")['answers'].items()[0]
+        term_comment = self.cycl_service.query_sentence(query, mt_monad="EnglishMt")['answers']['[Explain]']
         return term_comment
 
     def __init_predicate_comment(self) -> str:
         query = f"(comment #$Predicate ?TEXT)"
-        return self.cycl_service.query_sentence(query, mt_monad="BaseKB")['answers'].items()[0]
+        return self.cycl_service.query_sentence(query, mt_monad="BaseKB")['answers']['[Explain]']
 
     def __init_function_comment(self) -> str:
         query = f"(comment #$CollectionDenotingFunction ?TEXT)"
-        return self.cycl_service.query_sentence(query, mt_monad="BaseKB")['answers'].items()[0]
+        return self.cycl_service.query_sentence(query, mt_monad="BaseKB")['answers']['[Explain]']
 
     def __init_predicates(self) -> List:
         query = f"(#$isa {self.term.capitalize()} #$Predicate)"  # returns Query is not proven when unlinked
         answers = self.cycl_service.query_sentence(query, mt_monad="BaseKB")['answers']
-        if answers.keys() < 1:
+        if not answers:
             return self.__link_predicates()
         else:
             return answers
@@ -58,7 +62,7 @@ class CycSynset(Synset):
     def __init_functions(self) -> List:
         query = f"(#$isa {self.term.capitalize()} #$CollectionDenotingFunction)"
         answers = self.cycl_service.query_sentence(query, mt_monad="BaseKB")['answers']
-        if answers.keys() < 1:
+        if not answers:
             return self.__link_functions()
         else:
             return answers
