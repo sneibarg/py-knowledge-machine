ab742f11b030e27f70f13f67c85d518110d5555c
Scott Neibarger <scott.neibarger@gmail.com>
2025-09-22 17:57:25 -0400

    typing is nice.

--- word-to-krl.py
+++ word-to-krl.py
@@ -1,8 +1,9 @@
 import os
+from logging import Logger
 
 from nltk.stem import WordNetLemmatizer
 from agent.CycReasoningAgent import CycReasoningAgent
-from processor.nlp import TreeGenerator, translate_parse_tree, reconstruct_hyphenated_noun
+from processor.nlp import TreeGenerator, translate_parse_tree, reconstruct_hyphenated_noun, Node
 from processor.nlp.CycSynset import get_cyc_synset
 from service.LoggingService import LoggingService
 from service.NlpService import NlpService
@@ -10,26 +11,25 @@ from service.OllamaService import OllamaService
 from service.OpenCycService import OpenCycService
 from service.WordNetService import WordNetService
 
-test_sentence = "if you can't sit up while laying flat then you could stand to do some sit-ups"
-wnl = WordNetLemmatizer()
-payload = {"word": "sit-up", "pos": "noun"}
-wordnet_api = "http://dragon:9081/wordnet/synsets"
-nlp_api_url = "http://dragon:9081/nlp"
-ollama_api_url = "http://localhost:11434/api/generate"
-cyc_host = "dragon:3602"
-logging_service = LoggingService(os.path.join(os.getcwd(), "runtime", "logs"), "word-to-KRL")
-logger = logging_service.setup_logging(False)
-wordnet_service = WordNetService(logger)
-open_cyc_service = OpenCycService(cyc_host, logger)
-ollama_service = OllamaService(ollama_api_url, logger)
-nlp_service = NlpService(nlp_api_url, logger)
-cyc_reasoning_agent = CycReasoningAgent(ollama_service, open_cyc_service, logger)
-relations = nlp_service.stanford_relations(test_sentence)
-test_sentence = "if you can't sit up while laying flat then you could stand to do some sit-ups"
-# parse_tree = "(ROOT (S (SBAR (IN if) (S (NP (PRP you)) (VP (MD ca) (RB n't) (VP (VB sit) (PRT (RP up)) (PP (IN while) (S (VP (VBG laying) (ADJP (JJ flat))))))))) (ADVP (RB then)) (NP (PRP you)) (VP (MD could) (VP (VB stand) (S (VP (TO to) (VP (VB do) (NP (DT some) (NN sit) (HYPH -) (NNS ups)))))))))"
-parse_tree = str(relations['sentences'][0]['parseTree'])
-generator = translate_parse_tree(parse_tree, print_tree=True)
-parent_phrase = generator.get_parent_phrase('HYPH')
+test_sentence: str = "if you can't sit up while laying flat then you could stand to do some sit-ups"
+wnl: WordNetLemmatizer = WordNetLemmatizer()
+payload: dict = {"word": "sit-up", "pos": "noun"}
+wordnet_api: str = "http://dragon:9081/wordnet/synsets"
+nlp_api_url: str = "http://dragon:9081/nlp"
+ollama_api_url: str = "http://localhost:11434/api/generate"
+cyc_host: str = "dragon:3602"
+logging_service: LoggingService = LoggingService(os.path.join(os.getcwd(), "runtime", "logs"), "word-to-KRL")
+logger: Logger = logging_service.setup_logging(False)
+wordnet_service: WordNetService = WordNetService(logger)
+open_cyc_service: OpenCycService = OpenCycService(cyc_host, logger)
+ollama_service: OllamaService = OllamaService(ollama_api_url, logger)
+nlp_service: NlpService = NlpService(nlp_api_url, logger)
+cyc_reasoning_agent: CycReasoningAgent = CycReasoningAgent(ollama_service, open_cyc_service, logger)
+relations: dict = nlp_service.stanford_relations(test_sentence)
+test_sentence: str = "if you can't sit up while laying flat then you could stand to do some sit-ups"
+parse_tree: str = str(relations['sentences'][0]['parseTree'])
+generator: TreeGenerator = translate_parse_tree(parse_tree, print_tree=True)
+parent_phrase: Node = generator.get_parent_phrase('HYPH')
 
 if parent_phrase:
     node, reconstructed_noun = reconstruct_hyphenated_noun(parent_phrase)
@@ -52,5 +52,7 @@ if parent_phrase:
         print(ae)
     except ValueError:
         print(f"CycSynset for term {singular} does not exist.")
+        sentence: str = None
+        open_cyc_service.assert_sentence(sentence)
 else:
     print("No parent phrase node containing HYPH found.")
